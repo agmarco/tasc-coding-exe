06a80ab409939161065f0484f4b9ae6b5c479d21
Marco Aguirre <aguirreg.marco@gmail.com>
2019-06-23 01:11:17 -0500

    some tests

---
+++ src/test/store.unit.spec.ts
@@ -0,0 +1,17 @@
+import { getStore } from "../providers/storeProvider";
+
+describe("store test", () => {
+    const str = getStore();
+    it("should create a store", () => {
+        expect(str).not
+        .toBe(null);
+    });
+    it("should create products in  a store", () => {
+        expect(str.products).not
+        .toBe(null);
+    });
+    it("should create taxrates in  a store", () => {
+        expect(str.taxrates).not
+        .toBe(null);
+    });
+});
